1a195a55fb8be05a76b99cb18fbb9bb7487c7f86
Marcos Silva <w271592m@motorola.com>
2025-10-29 11:33:33 -0400

    feat: ensure BcryptAdapter returns a hash on success

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -4,7 +4,6 @@ import bcrypt from "bcrypt";
 export class BcryptAdapter implements Encrypter {
     constructor(private salt: number) {}
    async encrypt(value: string): Promise<string> {
-       await bcrypt.hash(value, this.salt);
-        return null;
+       return bcrypt.hash(value, this.salt);
     }
 }
